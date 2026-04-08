d8a1c10690e7da33b1f325effdf4903f51f09570
kacperfl <kacperfl29@gmail.com>
2026-04-08 13:56:09 +0200

    een voorbeeld van een template code

--- school/les_2.cpp
+++ school/les_2.cpp
@@ -4,6 +4,18 @@
 
 
 
+template<typename T>
+T add(T a, T b){
+    return a + b;
+}
+
 int main(){
-    
+    int a = 3;
+    int b = 5;
+
+    std::string x = "hallo";
+    std::string z = "doei";
+
+    std::cout << add(a, b) << std::endl;
+    std::cout << add(x, z);
 }
