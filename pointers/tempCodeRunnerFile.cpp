33484be232dbb57ec60ce855f20007d823fde579
kacperfl <kacperfl29@gmail.com>
2026-04-18 19:31:59 +0200

    meer pointer werk + arrys

--- pointers/tempCodeRunnerFile.cpp
+++ pointers/tempCodeRunnerFile.cpp
@@ -26,19 +26,12 @@ int opdracht_3(int *arr, int size){
     int *p = arr;
     int biggest_value = *p;
 
-    for(int i = 1; i < size; i++){
+    for(int i = 0; i < size; i++){
         if(*(p + i) > biggest_value){
             biggest_value = *(p + i);
         }
     }
     return biggest_value;
-}
-
-void extra(int *arr, int size){
-    int *p = arr;
-    for(int i = 0; i < size; i++){
-        *(p + i) *= 2;
-    }
 }
 
 int main(){
@@ -47,5 +40,4 @@ int main(){
     opdracht_1();
     std::cout<< opdracht_2(arr, 7) << std::endl;
     std::cout<< opdracht_3(arr, 7) << std::endl;
-    extra(arr, 7);
 }
